d888528c2db9d9974961d2b4d5ac1a148ce18155
Aditya Mattos <aditya.mattos@shopify.com>
2020-04-15 20:10:35 +0530

    Add shop parameter when redirecting after callback

--- app/controllers/shopify_app/callback_controller.rb
+++ app/controllers/shopify_app/callback_controller.rb
@@ -17,7 +17,7 @@ module ShopifyApp
         install_scripttags
         perform_after_authenticate_job
 
-        redirect_to(return_address)
+        redirect_to(return_address_with_params(shop: current_shopify_domain))
       else
         flash[:error] = I18n.t('could_not_log_in')
         redirect_to(login_url_with_optional_shop)
